e3fc41409fb91968133f52257e2561f66de40176
wessellourens <wessel7.lourens@gmail.com>
2019-08-26 10:05:59 +0200

    Added Maps

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,20 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+
 import { HomePage } from './home.page';
+import { AgmCoreModule } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    AgmSnazzyInfoWindowModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCTBMNAz5_IhGsemzmMmNnBwWB9nOoMUO4'
+    }),
     RouterModule.forChild([
       {
         path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  title = 'My first AGM project';
+  lat = -26.72104325806566;
+  lng = 27.09517283257685;
+  locationChosen = false;
+
   constructor() {}
+
+  onChoseLocation(event){
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+    this.locationChosen = true;
+  }
 
 }
