93788eb163d2526b15a945919c132a1f227f3277
Meyer Olivier <meyerolivier98@gmail.com>
2019-09-01 21:06:30 +0200

    Added takePhoto()

--- Camera/src/app/home/home.page.ts
+++ Camera/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SafeResourceUrl} from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 import { Plugins, CameraResultType, CameraSource} from '@capacitor/core';
 @Component({
@@ -9,11 +9,19 @@ import { Plugins, CameraResultType, CameraSource} from '@capacitor/core';
 })
 export class HomePage {
   image: SafeResourceUrl;
-  constructor() {}
+  constructor(private domSanitizer: DomSanitizer) {}
 
-  async takePhoto(){
-    const{Camera } =Plugins;
+  async takePhoto() {
+    const{Camera } = Plugins;
 
-    const result = Camera.getPhoto();
+    const result = await Camera.getPhoto({
+      quality: 75,
+      allowEditing: true,
+      source: CameraSource.Camera,
+      resultType: CameraResultType.Base64
+    });
+
+    this.image = this.domSanitizer.bypassSecurityTrustResourceUrl(result && result.base64String,
+      );
   }
 }
